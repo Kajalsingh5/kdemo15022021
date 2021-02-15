6a8aae375881bd508a1c346773f8951cc84e7a45
Kajalsingh5 <kajal.eternussolutions@gmail.com>
2021-02-15 13:48:22 +0530

    comit

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -71,7 +71,7 @@ define([
 
         payload['arguments'].execute.inArguments = [{
             "tokens": authTokens,
-            "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}"
+            "emailAddress": "{{Contact.Attribute.Age_below_20.EmailAddress}}"
         }];
         
         payload['metaData'].isConfigured = true;
